0666a218b825c6397299c163614f0a8d1ac06d83
xacker <egor_kinderr@mail.ru>
2022-07-02 23:33:35 +0300

    change name file with tests

--- test_google_should_find_selene.py
+++
@@ -1,8 +0,0 @@
-from selene.support.shared import browser
-from selene import be, have
-
-
-def test_google_should_find_selene():
-    browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
-    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
